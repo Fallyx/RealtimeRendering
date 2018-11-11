41d1d7847a31d8ad6589a09a49b928f8a2283d6d
Janis <Fallyx@users.noreply.github.com>
2018-11-11 17:40:21 +0100

    WIP

--- RealtimeRendering/MainWindow.xaml.cs
+++ RealtimeRendering/MainWindow.xaml.cs
@@ -14,25 +14,33 @@ namespace RealtimeRendering
     public partial class MainWindow : Window
     {
         private float alpha = 0;
-        private Triangle[] triangles;
+        private Triangle[][] triangles = new Triangle[2][];
         private WriteableBitmap wbmap;
         private byte[] pixels1d;
+        private Vector3[] colorBuff1d;
         private float[] zBuff1d;
+        private Vector3[] normalBuff1d;
+        private Vector3[] posBuff1d;
 
         private static int winWidth = 440;
         private static int winHeight = 440;
         private float zNear = float.PositiveInfinity;
         private float zFar = float.NegativeInfinity;
 
+        Vector3 Light = new Vector3(0, 5, -4);
         Vector3 Eye = new Vector3(0, 0, -4);
 
         public MainWindow()
         {
             InitializeComponent();
 
-            triangles = CreateTriangles();
+            triangles[0] = CreateTriangles(new Vector3(0, 0, 1));
+            triangles[1] = CreateTriangles(new Vector3(1, 0, 0));
             pixels1d = new byte[winWidth * winHeight * 4];
+            colorBuff1d = new Vector3[winWidth * winHeight];
             zBuff1d = new float[winWidth * winHeight];
+            normalBuff1d = new Vector3[winWidth * winHeight];
+            posBuff1d = new Vector3[winWidth * winHeight];
             
             wbmap = new WriteableBitmap(
                 winWidth,
@@ -56,12 +64,15 @@ namespace RealtimeRendering
             Array.Clear(pixels1d, 0, pixels1d.Length);
             Array.Clear(zBuff1d, 0, zBuff1d.Length);
 
-            for (int idx = 0; idx < 2; idx++)
+            for (int idx = 0; idx < triangles.Length; idx++)
             {
+                if (idx == 1) continue;
+
                 Matrix4x4 rot = Matrix4x4.CreateFromAxisAngle(new Vector3(0, 1, 0), (float)ToRad(alpha));
                 Matrix4x4 rot2 = Matrix4x4.CreateFromAxisAngle(new Vector3(1, 0, 0), (float)ToRad(alpha));
-                Matrix4x4 transRight = Matrix4x4.CreateTranslation(new Vector3(2, 0, 0));
+                Matrix4x4 transRight = Matrix4x4.CreateTranslation(new Vector3(3, 0, 0));
                 Matrix4x4 scale = Matrix4x4.CreateScale(0.5f);
+                Matrix4x4 transZ = Matrix4x4.CreateTranslation(new Vector3(0, 0, 3));
                 Matrix4x4 transl = Matrix4x4.CreateTranslation(new Vector3(0, 0, 5));
                 Matrix4x4 M;
 
@@ -71,67 +82,69 @@ namespace RealtimeRendering
                 }
                 else
                 {
-                    M = rot * rot2 * transRight * scale * transl * ProjectionMatrix();
+                    M = rot * rot2 * transRight * scale * transZ * transl * ProjectionMatrix();
                 }
-                
-
-
 
                 for (int i = 0; i < zBuff1d.Length; i++)
                 {
                     zBuff1d[i] = float.PositiveInfinity;
                 }
 
-                foreach (Triangle triangle in triangles)
+                foreach (Triangle triangle in triangles[idx])
                 {
-                    Matrix4x4 mZ = rot * rot2 * transl;
-
-                    Vector3 _pA = MathHelper.Transform(triangle.PointA, mZ);
-                    Vector3 _pB = MathHelper.Transform(triangle.PointB, mZ);
-                    Vector3 _pC = MathHelper.Transform(triangle.PointC, mZ);
-
-
                     Vector3 pA = MathHelper.Transform(triangle.PointA, M);
                     Vector3 pB = MathHelper.Transform(triangle.PointB, M);
                     Vector3 pC = MathHelper.Transform(triangle.PointC, M);
 
-                    Vector3 backface = Vector3.Cross(pB - pA, pC - pA);
-                    if (backface.Z < 0) continue;
-
-                    Triangle2D t2d = new Triangle2D(new Vector(pA.X, pA.Y), new Vector(pB.X, pB.Y), new Vector(pC.X, pC.Y), triangle.ColorA, triangle.ColorB, triangle.ColorC, backface.Z);
-
-                    //Vector AB = t2d.PointB - t2d.PointA;
-                    //Vector AC = t2d.PointC - t2d.PointA;
+                    //Triangle t = new Triangle(pA, pB, pC, triangle.Normal, triangle.ColorA, triangle.ColorB, triangle.ColorC);
 
                     Vector3 AB = pB - pA;
                     Vector3 AC = pC - pA;
 
-                    Vector3 _AB = _pB - _pC;
-                    Vector3 _AC = _pC - _pA;
+                    Vector3 backface = Vector3.Cross(new Vector3(AB.X, AB.Y, 0), new Vector3(AC.X, AC.Y, 0));
+                    if (backface.Z < 0) continue;
+
+                    Triangle2D t2d = new Triangle2D(new Vector(pA.X, pA.Y), new Vector(pB.X, pB.Y), new Vector(pC.X, pC.Y), triangle.ColorA, triangle.ColorB, triangle.ColorC, backface.Z);
+
+                    //t.TransformNormal(M);
+                    //t.SetColorsH(backface.Z);
+
+                    //Vector3 AB = t.PointB - t.PointA;
+                    //Vector3 AC = t.PointC - t.PointA;
 
                     Matrix2x2 m = new Matrix2x2(AB.X, AC.X, AB.Y, AC.Y);
                     Matrix2x2 invM = Matrix2x2.Inverse(m);
+
+                    //t.CalcMinMax();
 
                     for (int py = t2d.MinY; py < t2d.MaxY; py++)
                     {
                         for (int px = t2d.MinX; px < t2d.MaxX; px++)
                         {
-                            //Vector AP = new Vector(px, py) - t2d.PointA;
+                            //Vector2 AP = new Vector2(px - t.PointA.X, py - t.PointA.Y);
                             Vector3 AP = new Vector3(px, py, 0) - pA;
                             Vector uv = new Vector(invM.M11 * AP.X + invM.M12 * AP.Y, invM.M21 * AP.X + invM.M22 * AP.Y);
 
                             if (uv.X >= 0 && uv.Y >= 0 && (uv.X + uv.Y) < 1)
                             {
-                                float depth = (float)(_pA.Z + _AB.Z * uv.X + _AC.Z * uv.Y);
+                                //float depth = (float)(pA.Z + AB.Z * uv.X + AC.Z * uv.Y);
+                                Vector3 P = triangle.GetPoint((float)uv.X, (float)uv.Y);
+                                float depth = P.Z;
 
-                                if (zBuff1d[py * winWidth + px] > depth)
+                                int buffIdx = py * winWidth + px;
+
+                                if (zBuff1d[buffIdx] > depth)
                                 {
-                                    zBuff1d[py * winWidth + px] = depth;
+                                    zBuff1d[buffIdx] = depth;
                                     zNear = Math.Min(zNear, depth);
                                     zFar = Math.Max(zFar, depth);
 
-                                    //Vector3 c = t2d.InterpolateColor((float)uv.X, (float)uv.Y);
+                                    Vector3 c = t2d.InterpolateColor((float)uv.X, (float)uv.Y);
 
+                                    colorBuff1d[buffIdx] = c;
+                                    Vector3 norm = Vector3.TransformNormal(triangle.Normal, M); // triangle.TransformNormal(M);
+                                    normalBuff1d[buffIdx] = norm;
+                                    posBuff1d[buffIdx] = P;
                                     //SavePixel1d((py * winWidth + px) * 4, c);
                                 }
                             }
@@ -140,15 +153,31 @@ namespace RealtimeRendering
                 }
             }
 
+            
             for (int y = 0; y < winHeight; y++)
             {
                 for(int x = 0; x < winWidth; x++)
                 {
-                    float f = zBuff1d[y * winWidth + x];
+                    int buffIdx = y * winWidth + x;
+                    float f = zBuff1d[buffIdx];
                     if (!float.IsInfinity(f))
-                        SavePixel1dZ((y * winWidth + x) * 4, f);
+                    {
+                        Vector3 normal = normalBuff1d[buffIdx];
+                        Vector3 pos = posBuff1d[buffIdx];
+                        Vector3 c = colorBuff1d[buffIdx];
+
+                        // Vector3 diff = Diffuse(pos, normal, c);
+                        Vector3 PL = Vector3.Normalize(Light - pos);
+                        float diff = Math.Max(Vector3.Dot(normal, PL), 0);
+                        float spec = Specular(pos, normal);
+
+                        Vector3 pxlClr = new Vector3(0.1f, 0.1f, 0.1f) * new Vector3(c.X + diff, c.Y + diff, c.Z + diff) * new Vector3(c.X + spec, c.Y + spec, c.Z + spec);
+
+                        SavePixel1d(buffIdx * 4, pxlClr);
+                    }
                 }
             }
+            
 
             Int32Rect rect = new Int32Rect(0, 0, winWidth, winHeight);
             int stride = 4 * winWidth;
@@ -157,11 +186,40 @@ namespace RealtimeRendering
             alpha = (alpha + 1) % 360;
         }
 
+        private Vector3 Diffuse(Vector3 point, Vector3 normal, Vector3 color)
+        {
+            Vector3 diff = Vector3.Zero;
+            Vector3 lightClr = new Vector3(0.8f, 0.8f, 0.8f);
+
+            Vector3 PL = Vector3.Normalize(Light - point);
+
+            float nL = Vector3.Dot(normal, PL);
+
+            if(nL >= 0)
+            {
+                Vector3 il = Vector3.Multiply(lightClr, color);
+                diff = Vector3.Multiply(il, nL);
+            }
+
+            return diff;
+        }
+
+        private float Specular(Vector3 point, Vector3 normal)
+        {
+            Vector3 PL = Vector3.Normalize(Light - point);
+            Vector3 spec = Vector3.Normalize(2 * Vector3.Dot(PL, normal) * normal - PL);
+            Vector3 EL = Vector3.Normalize(Eye - point);
+
+            return (float) Math.Pow(Math.Min(0, Vector3.Dot(spec, EL)), 50);
+        }
+
         private void SavePixel1d(int index, Vector3 color)
         {
-            pixels1d[index] = (byte)(color.X * 255); // b
-            pixels1d[index + 1] = (byte)(color.Y * 255); // g
-            pixels1d[index + 2] = (byte)(color.Z * 255); // r
+            Color c = Color.FromScRgb(1, color.Z, color.Y, color.X);
+
+            pixels1d[index] = c.B; // (byte)(color.X * 255); // b
+            pixels1d[index + 1] = c.G; // (byte)(color.Y * 255); // g
+            pixels1d[index + 2] = c.R;  //(byte)(color.Z * 255); // r
             pixels1d[index + 3] = 255;
         }
 
@@ -300,6 +358,7 @@ namespace RealtimeRendering
             Vector3 normalFront = new Vector3(0, 0, -1);
             Vector3 normalBack = new Vector3(0, 0, 1);
 
+            
             triangles[0] = new Triangle(cubePts[(int)triangleIdx[0].X], cubePts[(int)triangleIdx[0].Y], cubePts[(int)triangleIdx[0].Z], normalTop, new Vector3(0, 0, 1), new Vector3(0, 0, 0.8f), new Vector3(0, 0, 0.8f));
             triangles[1] = new Triangle(cubePts[(int)triangleIdx[1].X], cubePts[(int)triangleIdx[1].Y], cubePts[(int)triangleIdx[1].Z], normalTop, new Vector3(0, 0, 0.8f), new Vector3(0, 0, 1), new Vector3(0, 0, 0.8f));
 
@@ -317,7 +376,72 @@ namespace RealtimeRendering
 
             triangles[10] = new Triangle(cubePts[(int)triangleIdx[10].X], cubePts[(int)triangleIdx[10].Y], cubePts[(int)triangleIdx[10].Z], normalBack, new Vector3(0.95f, 0.25f, 0.76f), new Vector3(0.79f, 0.25f, 0.95f), new Vector3(0.79f, 0.25f, 0.95f));
             triangles[11] = new Triangle(cubePts[(int)triangleIdx[11].X], cubePts[(int)triangleIdx[11].Y], cubePts[(int)triangleIdx[11].Z], normalBack, new Vector3(0.79f, 0.25f, 0.95f), new Vector3(0.95f, 0.25f, 0.76f), new Vector3(0.79f, 0.25f, 0.95f));
+            
 
+            return triangles;
+        }
+
+        private Triangle[] CreateTriangles(Vector3 color)
+        {
+            Vector3[] cubePts = new Vector3[]
+            {
+                // top
+                new Vector3(-1, -1, -1),
+                new Vector3(1, -1, -1),
+                new Vector3(1, 1, -1),
+                new Vector3(-1, 1, -1),
+
+                // bottom
+                new Vector3(-1, -1, 1),
+                new Vector3(1, -1, 1),
+                new Vector3(1, 1, 1),
+                new Vector3(-1, 1, 1)
+            };
+
+            Vector3[] triangleIdx = new Vector3[]
+            {
+                new Vector3(0, 1, 2), // top
+                new Vector3(0, 2, 3),
+                new Vector3(7, 6, 5), // bottom
+                new Vector3(7, 5, 4),
+                new Vector3(0, 3, 7), // left
+                new Vector3(0, 7, 4),
+                new Vector3(2, 1, 5), // right
+                new Vector3(2, 5, 6),
+                new Vector3(3, 2, 6), // front
+                new Vector3(3, 6, 7),
+                new Vector3(1, 0, 4), // back
+                new Vector3(1, 4, 5)
+            };
+
+            Triangle[] triangles = new Triangle[triangleIdx.Length];
+
+            Vector3 normalTop = new Vector3(0, 1, 0);
+            Vector3 normalBottom = new Vector3(0, -1, 0);
+            Vector3 normalLeft = new Vector3(-1, 0, 0);
+            Vector3 normalRight = new Vector3(1, 0, 0);
+            Vector3 normalFront = new Vector3(0, 0, -1);
+            Vector3 normalBack = new Vector3(0, 0, 1);
+
+            
+            triangles[0] = new Triangle(cubePts[(int)triangleIdx[0].X], cubePts[(int)triangleIdx[0].Y], cubePts[(int)triangleIdx[0].Z], normalTop, color, color, color);
+            triangles[1] = new Triangle(cubePts[(int)triangleIdx[1].X], cubePts[(int)triangleIdx[1].Y], cubePts[(int)triangleIdx[1].Z], normalTop, color, color, color);
+
+            triangles[2] = new Triangle(cubePts[(int)triangleIdx[2].X], cubePts[(int)triangleIdx[2].Y], cubePts[(int)triangleIdx[2].Z], normalBottom, color, color, color);
+            triangles[3] = new Triangle(cubePts[(int)triangleIdx[3].X], cubePts[(int)triangleIdx[3].Y], cubePts[(int)triangleIdx[3].Z], normalBottom, color, color, color);
+
+            triangles[4] = new Triangle(cubePts[(int)triangleIdx[4].X], cubePts[(int)triangleIdx[4].Y], cubePts[(int)triangleIdx[4].Z], normalLeft, color, color, color);
+            triangles[5] = new Triangle(cubePts[(int)triangleIdx[5].X], cubePts[(int)triangleIdx[5].Y], cubePts[(int)triangleIdx[5].Z], normalLeft, color, color, color);
+
+            triangles[6] = new Triangle(cubePts[(int)triangleIdx[6].X], cubePts[(int)triangleIdx[6].Y], cubePts[(int)triangleIdx[6].Z], normalRight, color, color, color);
+            triangles[7] = new Triangle(cubePts[(int)triangleIdx[7].X], cubePts[(int)triangleIdx[7].Y], cubePts[(int)triangleIdx[7].Z], normalRight, color, color, color);
+
+            triangles[8] = new Triangle(cubePts[(int)triangleIdx[8].X], cubePts[(int)triangleIdx[8].Y], cubePts[(int)triangleIdx[8].Z], normalFront, color, color, color);
+            triangles[9] = new Triangle(cubePts[(int)triangleIdx[9].X], cubePts[(int)triangleIdx[9].Y], cubePts[(int)triangleIdx[9].Z], normalFront, color, color, color);
+
+            triangles[10] = new Triangle(cubePts[(int)triangleIdx[10].X], cubePts[(int)triangleIdx[10].Y], cubePts[(int)triangleIdx[10].Z], normalBack, color, color, color);
+            triangles[11] = new Triangle(cubePts[(int)triangleIdx[11].X], cubePts[(int)triangleIdx[11].Y], cubePts[(int)triangleIdx[11].Z], normalBack, color, color, color);
+            
             return triangles;
         }
     }
--- RealtimeRendering/Models/Triangle.cs
+++ RealtimeRendering/Models/Triangle.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System;
+using System.Numerics;
 using System.Windows.Media;
 using RealtimeRendering.Helpers;
 
@@ -6,28 +7,31 @@ namespace RealtimeRendering
 {
     public class Triangle
     {
+        #region Vars
         private Vector3 pointA;
         private Vector4 pointAH;
         private Vector3 colorA;
+        private Vector4 colorAH;
 
         private Vector3 pointB;
         private Vector4 pointBH;
         private Vector3 colorB;
+        private Vector4 colorBH;
 
         private Vector3 pointC;
         private Vector4 pointCH;
         private Vector3 colorC;
+        private Vector4 colorCH;
 
         private Vector3 normal;
+        private Vector3 normalH;
         private Brush bColor;
 
-        public Triangle(Vector3[] points, Brush bColor)
-        {
-            PointA = points[0];
-            PointB = points[1];
-            PointC = points[2];
-            BColor = bColor;
-        }
+        private int minX;
+        private int maxX;
+        private int minY;
+        private int maxY;
+        #endregion
 
         public Triangle(Vector3 pointA, Vector3 pointB, Vector3 pointC, Brush bColor)
         {
@@ -51,19 +55,20 @@ namespace RealtimeRendering
         {
             PointA = pointA;
             ColorA = colorA;
-            PointAH = new Vector4(pointA.X / pointA.Z, pointA.Y / pointA.Z, pointA.Z / pointA.Z, 1 / pointA.Z);
+            PointAH = new Vector4(pointA / pointA.Z, 1 / pointA.Z);
 
             PointB = pointB;
             ColorB = colorB;
-            PointBH = new Vector4(pointB.X / pointB.Z, pointB.Y / pointB.Z, pointB.Z / pointB.Z, 1 / pointB.Z);
+            PointBH = new Vector4(pointB / pointB.Z, 1 / pointB.Z);
 
             ColorC = colorC;
             PointC = pointC;
-            PointCH = new Vector4(pointC.X / pointC.Z, pointC.Y / pointC.Z, pointC.Z / pointC.Z, 1 / pointC.Z);
+            PointCH = new Vector4(pointC / pointC.Z, 1 / pointC.Z);
 
             Normal = Vector3.Normalize(normal);
         }
 
+        #region getter and setters
         public Vector3 PointA { get => pointA; set => pointA = value; }
         public Vector3 PointB { get => pointB; set => pointB = value; }
         public Vector3 PointC { get => pointC; set => pointC = value; }
@@ -75,6 +80,51 @@ namespace RealtimeRendering
         public Vector4 PointAH { get => pointAH; set => pointAH = value; }
         public Vector4 PointBH { get => pointBH; set => pointBH = value; }
         public Vector4 PointCH { get => pointCH; set => pointCH = value; }
+        public Vector4 ColorAH { get => colorAH; }
+        public Vector4 ColorBH { get => colorBH; }
+        public Vector4 ColorCH { get => colorCH; }
+        public Vector3 NormalH { get => normalH; set => normalH = value; }
+        public int MinX { get => minX; set => minX = value; }
+        public int MaxX { get => maxX; set => maxX = value; }
+        public int MinY { get => minY; set => minY = value; }
+        public int MaxY { get => maxY; set => maxY = value; }
+
+        public void SetColorsH (float w)
+        {
+            colorAH = new Vector4(ColorA.X / w, ColorA.Y / w, ColorA.Z / w, 1 / w);
+            colorBH = new Vector4(ColorB.X / w, ColorB.Y / w, ColorB.Z / w, 1 / w);
+            colorCH = new Vector4(ColorC.X / w, ColorC.Y / w, ColorC.Z / w, 1 / w);
+        }
+
+
+        #endregion
+
+        public void CalcMinMax()
+        {
+            MinX = (int)Math.Min(PointA.X, Math.Min(PointB.X, PointC.X));
+            MaxX = (int)Math.Max(PointA.X, Math.Max(PointB.X, PointC.X));
+            MinY = (int)Math.Min(PointA.Y, Math.Min(PointB.Y, PointC.Y));
+            MaxY = (int)Math.Max(PointA.Y, Math.Max(PointB.Y, PointC.Y));
+        }
+
+        public Vector3 InterpolateColor(float u, float v)
+        {
+            Vector4 _colorPt = new Vector4(ColorAH.X + (u * (ColorBH.X - ColorAH.X)) + (v * (ColorCH.X - ColorAH.X)),
+                                          ColorAH.Y + (u * (ColorBH.Y - ColorAH.Y)) + (v * (ColorCH.Y - ColorAH.Y)),
+                                          ColorAH.Z + (u * (ColorBH.Z - ColorAH.Z)) + (v * (ColorCH.Z - ColorAH.Z)),
+                                          ColorAH.W + (u * (ColorBH.W - ColorAH.W)) + (v * (ColorCH.W - ColorAH.W)));
+
+            Vector3 colorPt = new Vector3(_colorPt.X / _colorPt.W, _colorPt.Y / _colorPt.W, _colorPt.Z / _colorPt.W);
+            return colorPt;
+        }
+
+        public Vector3 GetPoint(float u, float v)
+        {
+            Vector4 pt = PointAH + (PointBH - PointAH) * u + (PointCH - PointAH) * v;
+            pt /= pt.W;
+
+            return new Vector3(pt.X, pt.Y, pt.Z);
+        }
 
         public void TransformNormal(Matrix4x4 m)
         {
